ab187721ebb7d8efc1712eb50fa3ea8cdf60c843
Michael Carroll <carroll.michael@gmail.com>
2012-04-28 12:41:36 -0500

    Navigation updates.

--- automow_navigation/nodes/simple_navigation_goals.py
+++ automow_navigation/nodes/simple_navigation_goals.py
@@ -7,22 +7,25 @@ import rospy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 
-
 def movebase_client():
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-
     client.wait_for_server()
-
+    rospy.loginfo('got server')
     goal = MoveBaseGoal()
-
     goal.target_pose.header.frame_id = "base_link"
     goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.position.x = 1.0
-    goal.target_pose.orientation.w = 1.0
-
+    goal.target_pose.pose.position.x = -10.0
+    goal.target_pose.pose.position.y = 0.0
+    goal.target_pose.pose.position.z = 0.0
+    goal.target_pose.pose.orientation.x = 0.0
+    goal.target_pose.pose.orientation.y = 0.0
+    goal.target_pose.pose.orientation.z = 0.0
+    goal.target_pose.pose.orientation.w = 1.0
     client.send_goal(goal)
-
-    client.get_result()
+    rospy.loginfo('sent goal')
+    rospy.loginfo(goal)
+    client.wait_for_result()
+    return client.get_result()
 
 if __name__ == '__main__':
     try:
